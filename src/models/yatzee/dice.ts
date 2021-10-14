c1cb717c3016ada6ddab80d4d7affe4eed4ad167
Kevin Tourret <tarpion@toto.fr>
2021-10-14 14:48:27 +0200

    Préparation : selection dé pour relance

--- src/models/yatzee/dice.ts
+++ src/models/yatzee/dice.ts
@@ -3,10 +3,12 @@ export class Dice {
 
   private readonly _value: number;
   private readonly _image: string;
+  private _isSelected: boolean;
 
   constructor() {
     this._value = Math.floor(Math.random() * 6) + 1;
     this._image = 'assets/yatzee/' + this._value + '.png';
+    this._isSelected = false;
   }
 
   get value(): number {
@@ -17,4 +19,11 @@ export class Dice {
     return this._image;
   }
 
+  get isSelected(): boolean {
+    return this._isSelected;
+  }
+
+  set isSelected(value: boolean) {
+    this._isSelected = value;
+  }
 }

de4335ff3e2bd27b2491a6867e82990ccb9632db
Kevin Tourret <tarpion@toto.fr>
2021-10-18 08:56:42 +0200

    Save

--- src/models/yatzee/yahtzee.ts
+++ src/models/yatzee/yahtzee.ts
@@ -11,6 +11,21 @@ export class Yahtzee {
     }
   }
 
+  /**
+   * Fonction permettant de relancer les dés selectionnés
+   * (Ou les non-sélectionnés, au choix...)
+   */
+  rerollDice(): void {
+    // parcourir le tableau de dés
+    for (const index in this._myDice) {
+      // si mon dé est sélectionné
+      if (this._myDice[index].isSelected) {
+        // je re-créé le dé
+        this._myDice[index] = new Dice();
+      }
+    }
+  }
+
   get myDice(): Array<Dice> {
     return this._myDice;
   }

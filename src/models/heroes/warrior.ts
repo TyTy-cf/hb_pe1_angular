78d0aa1ab8a418264b48e806606aab54b5063356
Kevin Tourret <tourret.kevin@gmail.com>
2021-10-18 22:56:09 +0200

    exo component + input

---
+++ src/models/heroes/warrior.ts
@@ -0,0 +1,20 @@
+import {Hero} from "./hero";
+
+export class Warrior extends Hero {
+
+  constructor(name: string) {
+    super(name, 360, 42, 47, 4);
+  }
+
+  levelUp(): void {
+    // super est le mot clé pour appeler le comportement de la mère
+    super.levelUp();
+    this._hitPoint += 39;
+    this._damageMin += 2;
+    this._damageMax += 2;
+    if (this._level%3 === 0) {
+      this._defense += 1;
+    }
+  }
+
+}

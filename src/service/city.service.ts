c6553e7d2e4838f692c35bd4790b1e6635fe7e80
Kevin Tourret <tarpion@toto.fr>
2021-10-20 09:42:56 +0200

    Correction cities

--- src/service/city.service.ts
+++ src/service/city.service.ts
@@ -11,6 +11,8 @@ export class CityService {
     new City("63001", "Aigueperse", "63", 2720, ["63260"]),
     new City("63002", "Aix-la-Fayette", "63", 91, ["63980"]),
     new City("42187", "Roanne", "42", 34685, ["42300"]),
+    new City("63113", "Clermont-Ferrand", "63", 142686, ["63100", "63000"]),
+    new City("42218", "Saint-Etienne", "42", 171924, ["42000", "42100", "42230"])
   ];
 
   get arrayCity(): Array<City> {
